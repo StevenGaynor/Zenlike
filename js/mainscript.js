0bf3cdecbea15359e954174a0bc6573984cfceb6
StevenGaynor <sgaynor22@gmail.com>
2015-04-09 12:28:30 +0100

    JavaScript initial

---
+++ js/mainscript.js
@@ -0,0 +1,8 @@
+function init()
+{
+  var panel = document.getElementById( "panel" );
+  var dfn = "Test";
+  
+  panel.innerHTML = dfn;
+}
+document.addEventListener( "DOMContentLoaded", init, false);
