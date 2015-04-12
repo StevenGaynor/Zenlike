cd6e1a6e8f7471d96350ac1509e1814ee16025a8
StevenGaynor <sgaynor22@gmail.com>
2015-04-12 16:09:55 +0100

    starting php site

---
+++ register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "UTF-8">
+		<meta name="description" content="balancing Fitness, diet and mindfulness">
+		<meta name="keywords" content="zen, meditation, mindfulness, healthy, diet, exercise, balance">
+		<meta name="author" content="Steven Gaynor">
+		<title>Zenlike | Balancing Fitness, Diet and Mindfulness</title>
+	    <link rel="stylesheet" type="text/css" href="css/mainstyle.css" media="screen">
+		<link rel="stylesheet" type="text/css" href="css/print.css" media="print">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		
+	
+	    <!--responsive menu-->
+		
+	</head>
+	
+	<body>
+		<div id="outerWrapper">
+			<header>
+					<h1 id="mainLogo">Zenlike</h1>
+			</header>
+					
+			<nav id="topNav">
+					<ul id="topNavUl">
+						<li><a href="index.html" tabindex="1">Home</a></li> 
+						<li><a href="objectives.html" tabindex="2">What We Do</a></li>
+						<li><a href="guide.html" tabindex="3">Using Zenlike</a></li>
+						<li><a href="myhub.html" tabindex="4">MyHub</a></li>
+					</ul>
+			</nav>
+									
+			<main>
+				
+			</main>
+			
+			<footer>
+				<p class="createdBy">Zenlike | Created by Steven Gaynor | 2015</p>
+				<nav id="footerNav"><a href="contact.html">Contact Us</a> | About Us | Accessibility | Site Map</nav>
+			</footer>
+			
+		</div> <!--close outerWrapper-->
+	</body>
+</html>
