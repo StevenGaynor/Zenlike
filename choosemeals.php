dcb105748595b49874a9a7226c82c16b02da0cae
StevenGaynor <sgaynor22@gmail.com>
2015-04-12 16:43:30 +0100

    meals php test

--- choosemeals.php
+++ choosemeals.php
@@ -31,6 +31,39 @@
 			</nav>
 									
 			<main>
+			
+				<section id="test">
+					<h2>Test Data PHP</h2>
+					<?php
+						$servername = "localhost";
+						$username = "z564098";
+						$password = "oS7Y7pu50d";
+						$dbname = "z564098_Zenlike";
+
+						// Create connection
+						$conn = new mysqli($servername, $username, $password, $dbname);
+						// Check connection
+						if ($conn->connect_error) {
+							die("Connection failed: " . $conn->connect_error);
+						} 
+
+						$sql = "SELECT meal_ID, meal_name, meal_description, calorie_count FROM meals";
+						$result = $conn->query($sql);
+
+						if ($result->num_rows > 0) {
+							// output data of each row
+							while($row = $result->fetch_assoc()) {
+								echo "<b>Meal ID: </b>" . $row["meal_ID"]. " <b>Meal Name:</b> " . $row["meal_name"]. " <b>Description</b>  " . $row["meal_description"]. "<b>Calories  </b>". $row["calorie_count"]. "<br>";
+							}
+						} else {
+							echo "0 results";
+						}
+						$conn->close();
+					?>
+				</section>
+				<!--<table>
+					<tr>
+				</table>-->
 				
 			</main>
 			
