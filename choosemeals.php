5f48b527b795bb0fb00636f506f305ea98c9ce8e
StevenGaynor <sgaynor22@gmail.com>
2015-04-13 13:20:26 +0100

    further progress meals php
    added meal creator box, initial styling on meals data display

--- choosemeals.php
+++ choosemeals.php
@@ -32,8 +32,8 @@
 									
 			<main>
 			
-				<section id="test">
-					<h2>Test Data PHP</h2>
+				<section>
+					<h2>Choose a Meal</h2>
 					<?php
 						$servername = "localhost";
 						$username = "z564098";
@@ -47,23 +47,49 @@
 							die("Connection failed: " . $conn->connect_error);
 						} 
 
-						$sql = "SELECT meal_ID, meal_name, meal_description, calorie_count FROM meals";
+						$sql = "SELECT meal_ID, meal_name, meal_description, calorie_count, vegetarian, vegan FROM meals";
 						$result = $conn->query($sql);
 
 						if ($result->num_rows > 0) {
 							// output data of each row
 							while($row = $result->fetch_assoc()) {
-								echo "<b>Meal ID: </b>" . $row["meal_ID"]. " <b>Meal Name:</b> " . $row["meal_name"]. " <b>Description</b>  " . $row["meal_description"]. "<b>Calories  </b>". $row["calorie_count"]. "<br>";
+								echo " Meal Number " .$row["meal_ID"]. "Name" .$row["meal_name"]. " Description " . $row["meal_description"]. "Calories ". $row["calorie_count"]. " Vegetarian " . $row["vegetarian"]." Vegan " .$row["vegan"]. "<hr/>" ;
 							}
 						} else {
 							echo "0 results";
 						}
 						$conn->close();
 					?>
+											
 				</section>
-				<!--<table>
-					<tr>
-				</table>-->
+				<form class = "smart-green" action="createMealEvent.php" method="post">
+						<h1>Create your meal event here</h1>
+						<label><span>Enter meal number: </span><input id="meal_ID" type="number" name="meal_ID"></label>
+						<label><span>Enter Day: <select name="mealDay">
+							<option value="2015-04-05">Sunday</option>
+							<option value="2015-04-06">Monday</option>
+							<option value="2015-04-07">Tuesday</option>
+							<option value="2015-04-08">Wednesday</option>
+							<option value="2015-04-09">Thursday</option>
+							<option value="2015-04-10">Friday</option>
+							<option value="2015-04-11">Saturday</option>
+							</select>
+						</label>
+						<label><span>Enter Time: </span> <select name="mealTime">
+							<option value="06:00:00">6am - 9am</option>
+							<option value="09:00:00">9am - 12pm</option>
+							<option value="12:00:00">12pm - 3pm</option>
+							<option value="15:00:00">3pm - 6pm</option>
+							<option value="18:00:00">6pm - 9pm</option>
+							<option value="21:00:00">9pm - 12am</option>
+							</select>
+						</label>
+						<label>
+						<span>&nbsp;</span> 
+						<input type="button" class="button" value="Save"> 
+					</label>
+					</form>
+				
 				
 			</main>
 			
