e69502dd6f4a43902da638797fa0733cb82700bb
StevenGaynor <sgaynor22@gmail.com>
2015-04-17 13:24:39 +0100

    further work on choosemeals

--- choosemeals.php
+++ choosemeals.php
@@ -1,105 +1,76 @@
-<!DOCTYPE html>
-<html lang = "en">
-	<head>
-		<meta charset = "UTF-8">
-		<meta name="description" content="balancing Fitness, diet and mindfulness">
-		<meta name="keywords" content="zen, meditation, mindfulness, healthy, diet, exercise, balance">
-		<meta name="author" content="Steven Gaynor">
-		<title>Zenlike | Balancing Fitness, Diet and Mindfulness</title>
-	    <link rel="stylesheet" type="text/css" href="css/mainstyle.css" media="screen">
-		<link rel="stylesheet" type="text/css" href="css/print.css" media="print">
-		<meta name="viewport" content="width=device-width,initial-scale=1">
-		
-		<script type="text/javascript" src="js/mainscript.js"></script>
-		
-	</head>
-	
-	<body>
-		<div id="outerWrapper">
-			<header>
-					<h1 id="mainLogo">Zenlike</h1>
-			</header>
-					
-			<nav id="topNav">
-					<ul id="topNavUl">
-						<li><a href="index.html" tabindex="1">Home</a></li> 
-						<li><a href="objectives.html" tabindex="2">What We Do</a></li>
-						<li><a href="guide.html" tabindex="3">Using Zenlike</a></li>
-						<li><a href="myhub.php" tabindex="4">MyHub</a></li>
-					</ul>
-			</nav>
+<?php 
+
+session_start();
+
+if(!isset($_SESSION['per_ID']))
+{
+	require('login_tools.php');
+	load();
+}
+
+$page_title = 'Zenlike - Food';
+include ('includes/header.html');
+
+require('../connect_db.php');
 									
-			<main>
+	echo "<main>
 			
 				<section>
-					<h2>Our Healthy Meals</h2>
-					<?php
-						$servername = "localhost";
-						$username = "z564098";
-						$password = "oS7Y7pu50d";
-						$dbname = "z564098_Zenlike";
+					<h2>Our Healthy Meals</h2>";
+					echo "<p id='loggedInMsg'>Welcome <b><u>{$_SESSION['first_name']}&nbsp;{$_SESSION['surname']}</u></b>&nbsp;|&nbsp;<a id='myHubButton' href='myhub.php'>MyHub</a>&nbsp;|&nbsp;<span id='logoutButton'><a href='logout.php'>Logout</a></span></p>";
+						$q = "SELECT * FROM meals";
+						$r = mysqli_query($dbc, $q);
 
-						// Create connection
-						$conn = new mysqli($servername, $username, $password, $dbname);
-						// Check connection
-						if ($conn->connect_error) {
-							die("Connection failed: " . $conn->connect_error);
-						} 
-
-						$sql = "SELECT meal_ID, meal_name, meal_description, calorie_count, vegetarian, vegan FROM meals";
-						$result = $conn->query($sql);
-
-						if ($result->num_rows > 0) {
-							// output data of each row
-							while($row = $result->fetch_assoc()) {
-								echo " <b>Meal Number: </b> " .$row["meal_ID"]. " <b>Meal Name: </b> " .$row["meal_name"]. " <b>Description: </b> " . $row["meal_description"]. " <b>Calories: </b> ". $row["calorie_count"]. " <b>Vegetarian?: </b>" . $row["vegetarian"]." <b>Vegan?: </b> " .$row["vegan"]. "<hr/>" ;
+						if (mysqli_num_rows($r) > 0) {
+							while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
+							{
+								echo '<h3><strong>'.$row['meal_name'].
+									 '</strong></h3><p>'.$row['meal_description'].
+									 '<br><img style="float:right;" src='.$row['meal_img'].
+									 '><p>Calories:&nbsp;'.$row['calorie_count'].
+									 '</p><p>Vegetarian?:&nbsp;'.$row['vegetarian'].
+									 '</p><p>Vegan?:&nbsp;'.$row['vegan'].
+									 '</p><p><a href="addedMeal.php?id='.$row['meal_ID'].
+									 '">Save to MyCalendar</a></p>';
 							}
-						} else {
-							echo "0 results";
-						}
-						$conn->close();
-					?>
+							mysqli_close($dbc);
+						}else {echo'<p>No meals at present</p>';}
+						echo"
 											
-				</section>
-				<form class = "smart-green" action="createMealEvent.php" method="post">
+				</section>";
+				echo "
+				<form class = 'smart-green' action='createMealEvent.php' method='post'>
 						<h1>Create your meal event here<span>Choose a meal of your choice from above and when is best to go eat.</span></h1>
 						
-						<label><span>Enter meal number: </span><input id="meal_ID" type="number" min="1" max="6" name="meal_ID"></label>
+						<label><span>Enter meal number: </span><input id='meal_ID' type='number' min='1' max='6' name='meal_ID'></label>
 						<label><span>Enter Day: </span>
-							<select name="mealevent_day">
-								<option value="2015-04-05">Sunday</option>
-								<option value="2015-04-06">Monday</option>
-								<option value="2015-04-07">Tuesday</option>
-								<option value="2015-04-08">Wednesday</option>
-								<option value="2015-04-09">Thursday</option>
-								<option value="2015-04-10">Friday</option>
-								<option value="2015-04-11">Saturday</option>
+							<select name='mealevent_day'>
+								<option value='2015-04-05'>Sunday</option>
+								<option value='2015-04-06'>Monday</option>
+								<option value='2015-04-07'>Tuesday</option>
+								<option value='2015-04-08'>Wednesday</option>
+								<option value='2015-04-09'>Thursday</option>
+								<option value='2015-04-10'>Friday</option>
+								<option value='2015-04-11'>Saturday</option>
 							</select>
 						</label>
 						<label><span>Enter Time: </span>
-							<select name="mealevent_time">
-								<option value="06:00:00">6am - 9am</option>
-								<option value="09:00:00">9am - 12pm</option>
-								<option value="12:00:00">12pm - 3pm</option>
-								<option value="15:00:00">3pm - 6pm</option>
-								<option value="18:00:00">6pm - 9pm</option>
-								<option value="21:00:00">9pm - 12am</option>
+							<select name='mealevent_time'>
+								<option value='06:00:00'>6am - 9am</option>
+								<option value='09:00:00'>9am - 12pm</option>
+								<option value='12:00:00'>12pm - 3pm</option>
+								<option value='15:00:00'>3pm - 6pm</option>
+								<option value='18:00:00'>6pm - 9pm</option>
+								<option value='21:00:00'>9pm - 12am</option>
 							</select>
 						</label>
 						<label>
 							<span>&nbsp;</span> 
-							<input type="submit" class="button" value="Save"> 
+							<input type='submit' class='button' value='Save'> 
 						</label>
 					</form>
 				
 				
-			</main>
-			
-			<footer>
-				<p class="createdBy">Zenlike | Created by Steven Gaynor | 2015</p>
-				<nav id="footerNav"><a href="contact.html">Contact Us</a> | <a href="#">Site Map</a></nav>
-			</footer>
-			
-		</div> <!--close outerWrapper-->
-	</body>
-</html>
+			</main>";
+include('includes/footer.html');
+		
--- myhub.php
+++ myhub.php
@@ -6,11 +6,13 @@ if ( !isset( $_SESSION[ 'per_ID' ] ) ) { require ( 'login_tools.php' ) ; load() 
 
 $page_title = 'Zenlike - MyHub';
 include('includes/header.html');
-  
+
+require('../connect_db.php');
+
   echo  "<main>
 			<section id='zenlikeHubContainer'>
 				<h2>MyHub</h2>
-					<p id='loggedInMsg'>Welcome <b><u>{$_SESSION['first_name']}&nbsp;{$_SESSION['surname']}</u></b><span id='logoutButton'><a href='logout.php'>|&nbsp;Logout</a></span></p>";
+					<p id='loggedInMsg'>Welcome <b><u>{$_SESSION['first_name']}&nbsp;{$_SESSION['surname']}</u></b>&nbsp;|&nbsp;<a id='myHubButton' href='myhub.php'>MyHub</a>&nbsp;|&nbsp;<span id='logoutButton'><a href='logout.php'>Logout</a></span></p>";
 				
 					echo "	<section id ='zenlikeCalendar'>
 						<h3>Weekly Planner</h3>
@@ -19,7 +21,7 @@ include('includes/header.html');
 								<tr><th>Time</th><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>
 							</thead>
 							<tbody>
-								<tr><td>6am - 9am</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+								<tr><td>6am - 9am</td><td id='sunday6to9'>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>9am - 12pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>12pm - 3pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>3pm - 6pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
