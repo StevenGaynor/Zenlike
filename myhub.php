d6e2023261d045ce338d17459a8ff39485d4e837
StevenGaynor <sgaynor22@gmail.com>
2015-04-29 22:50:45 +0100

    first successful MyCalendar query from DB

--- myhub.php
+++ myhub.php
@@ -8,20 +8,48 @@ $page_title = 'Zenlike - MyHub';
 include('includes/header.html');
 
 require('../connect_db.php');
-
-  echo  "<main>
+$per_ID = $_SESSION['per_ID'];
+echo  "<main>
 			<section id='zenlikeHubContainer'>
 				<h2>MyHub</h2>
 					<p id='loggedInMsg'>Welcome <b><u>{$_SESSION['first_name']}&nbsp;{$_SESSION['surname']}</u></b>&nbsp;|&nbsp;<a id='myHubButton' href='myhub.php'>MyHub</a>&nbsp;|&nbsp;<span id='logoutButton'><a href='logout.php'>Logout</a></span></p>";
+?>
 				
-					echo "	<section id ='zenlikeCalendar'>
+					<section id ='zenlikeCalendar'>
 						<h3>Weekly Planner</h3>
 						<table>
 							<thead>
 								<tr><th>Time</th><th>Sunday</th><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th></tr>
 							</thead>
 							<tbody>
-								<tr><td>6am - 9am</td><td id='sunday6to9'>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+								<tr><td>6am - 9am</td><td><!--SUNDAY 6:00-->
+														
+														
+														<?php 
+														
+														$q = "SELECT meals.meal_name, mealEvents.mealevent_day, mealEvents.mealevent_time, mealEvents.per_ID
+															  FROM meals
+															  INNER JOIN mealEvents
+															  ON meals.meal_ID = mealEvents.meal_ID
+															  WHERE mealEvents.per_ID = '$per_ID'
+															  AND mealEvents.mealevent_day = '2015-04-05'
+															  AND mealEvents.mealevent_time = '06:00:00' ";
+														$r = mysqli_query($dbc, $q);
+														if (mysqli_num_rows($r) > 0)
+														{
+															while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC))
+															{
+															echo '<p><strong>'.$row['meal_name'].
+															'</strong></p>';
+															}mysqli_close($dbc);
+														}else {echo'&nbsp;';}
+														
+														?>
+														
+								
+								
+								
+								</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>9am - 12pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>12pm - 3pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
 								<tr><td>3pm - 6pm</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
@@ -38,7 +66,8 @@ require('../connect_db.php');
 					</section>
 					
 				</section>
-			</main>";
+			</main>
+<?php
 	include ('includes/footer.html');
 ?>
 	
